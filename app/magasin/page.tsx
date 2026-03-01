0b5dbe0181ea051b51fd2cfe0f38d92672afaab5
Israel Elie ADM N'DOUA <israelelieadm@CDCI.CI>
2026-03-01 18:32:26 +0000

    exercice_corigée

--- app/magasin/page.tsx
+++ app/magasin/page.tsx
@@ -3,17 +3,20 @@ export const dynamic = "force-dynamic"
 import MagasinsGrid from '@/components/Magasin/MagasinGrid'
 import MagasinsHeader from '@/components/Magasin/MagHeader'
 import { PaginationMagasins } from '@/components/Magasin/PaginationMagasins'
+import { Suspense } from 'react'
 
 
 const Magasin = () => {
   return (
     <div className="px-10 py-6 mx-5 ">
+<Suspense fallback={<div>Chargement...</div>}>
 
 <MagasinsHeader/>
 
 <MagasinsGrid/>
 
 <PaginationMagasins totalPages={4}/>
+</Suspense>
 
 </div>
 
